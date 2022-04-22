b9116100877937cc241a29101c743c3e957b26bc
alex <atroup@student.unimelb.edu.au>
2022-04-22 22:15:02 +1000

    redo folder structure

---
+++ web_application/README.md
@@ -0,0 +1,2 @@
+# Cluster_and_Cloud_Assign2
+Github Repository for Assignment 2
---
+++ web_application/services/web/flask/wsgi.py
@@ -0,0 +1,14 @@
+"""
+This file is the entrypoint for wsgi (web server gateway interface) initialisation.
+See: https://flask.palletsprojects.com/en/2.1.x/deploying/uwsgi/
+"""
+
+from os import getenv
+
+from app import init_app
+
+application = init_app()
+
+# if run directly
+if __name__ == "__main__":
+    application.run(host="0.0.0.0", port=getenv("port", 5000), debug=True)
