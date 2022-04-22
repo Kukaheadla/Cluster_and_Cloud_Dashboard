aa4fa59427e5a2a545299610ab6bb577c3be1e32
alex <atroup@student.unimelb.edu.au>
2022-04-22 22:15:32 +1000

    rm files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Cluster_and_Cloud_Assign2
-Github Repository for Assignment 2
--- docker_compose_start.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-# this file simply holds some commands that you might find useful
-
-# build and starts
-docker-compose -f docker-compose.prod.yml up -d --build
-
-# kills and removes containers
-# docker-compose -f docker-compose.prod.yml down -v   
