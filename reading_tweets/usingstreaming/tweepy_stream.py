f8a5da809b6bad9cc8bc851faa5df82220893ce0
DavidL124 <60912784+DavidL124@users.noreply.github.com>
2022-04-25 01:10:14 +1000

    Removed unnecessary code

--- reading_tweets/usingstreaming/tweepy_stream.py
+++ reading_tweets/usingstreaming/tweepy_stream.py
@@ -12,17 +12,6 @@ import os, json, datetime
 from flask import Flask
 import pandas as pd
 
-##Include class for reading in datetime to JSON:
-####https://stackoverflow.com/questions/56554159/typeerror-object-of-type-datetime-is-not-json-serializable-with-serialize-fu
-
-class DateTimeEncoder(json.JSONEncoder):
-    def default(self, z):
-        if isinstance(z, datetime.datetime):
-            return (str(z))
-        else:
-            return super().default(z)
-#### 
-
 ##Fields
 tweet_fields = ["attachments", "author_id", "context_annotations", "conversation_id", "created_at", "entities", "geo", "lang", "id", "text"]
 user_fields = ["name", "username", "location", "verified", "description"]
@@ -31,7 +20,7 @@ place_fields = ["contained_within", "country", "country_code", "geo", "name", "f
 ##
 
 app = Flask(__name__, static_url_path="")
-fp = open("stream_tweets.json", "a")
+fp = open("output.json", "a")
 
 class TweetListener(StreamingClient):
     """
@@ -39,15 +28,22 @@ class TweetListener(StreamingClient):
     https://docs.tweepy.org/en/latest/streamingclient.html#tweepy.StreamingClient
     """
     count = 0
-    limit = 3
+    limit = 5
+    result = []
+    tweet_dict = {}
     #Defining some variables:
     def on_tweet(self, tweet: Tweet):
-        json.dump(dict(tweet.data), fp, cls=DateTimeEncoder)
+        tmp = dict(tweet.data)
+        print(tmp)
+        if 'created_at' in tmp.keys() and tmp['created_at'] != None:
+            tmp['created_at'] = str(tmp['created_at'])
+        #json.dump(dict(tmp), fp, cls=DateTimeEncoder)
+        (TweetListener.result).append(tmp)
         print(tweet.__repr__()) #Prints out content of the tweet.
         TweetListener.count += 1
         if TweetListener.count >= TweetListener.limit:
+            json.dump(TweetListener.result, fp)
             self.disconnect()
-            fp.close()
 
     def on_request_error(self, status_code):
         print(status_code)
@@ -55,39 +51,9 @@ class TweetListener(StreamingClient):
     def on_connection_error(self):
         self.disconnect()
 
+def rule_regulation(client, rules):
 
-if __name__ == "__main__":
-    """
-     - Save it in a secure location
-     - Treat it like a password or a set of keys
-     - If security has been compromised, regenerate it
-     - DO NOT store it in public places or shared docs
-    """
-    #First obtain the necessary authorization data
-    consumer_key = os.environ.get("TWITTER_API_KEY")
-    consumer_secret = os.environ.get("TWITTER_API_KEY_SECRET")
-    access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
-    access_token_secret = os.environ.get("TWITTER_ACCESS_SECRET")
-    bearer_token = os.getenv("TWITTER_BEARER_TOKEN")
-
-    if not bearer_token:
-        raise RuntimeError("Not found bearer token")
-
-    client = TweetListener(bearer_token)
-
-    # https://docs.tweepy.org/en/latest/streamingclient.html#streamrule
-    # https://developer.twitter.com/en/docs/twitter-api/tweets/filtered-stream/integrate/build-a-rule
-    # Operator availability (check the operators table)
-    # - Core operators: Available when using any access level.
-    # - Advanced operators: Available when using a Project with Academic Research access.
-    # keyword:
-    #   - "melbourne"
-    rules = [
-        StreamRule(value="melbourne")
-    ]
-
-    # https://developer.twitter.com/en/docs/twitter-api/tweets/filtered-stream/api-reference/post-tweets-search-stream-rules
-    # Remove existing rules
+    #remove existing rules
     resp = client.get_rules()
     if resp and resp.data:
         rule_ids = []
@@ -105,13 +71,31 @@ if __name__ == "__main__":
     resp = client.add_rules(rules)
     if resp.errors:
         raise RuntimeError(resp.errors)
+    
+def main():
+    #First obtain the necessary authorization data
+    bearer_token = os.getenv("TWITTER_BEARER_TOKEN")
 
+    if not bearer_token:
+        raise RuntimeError("Not found bearer token")
+
+    client = TweetListener(bearer_token)
+
+    rules = [
+            StreamRule(value="melbourne")
+    ]   
+    rule_regulation(client, rules)
     # https://developer.twitter.com/en/docs/twitter-api/tweets/filtered-stream/api-reference/get-tweets-search-stream-rules
     print(client.get_rules())
 
     # https://developer.twitter.com/en/docs/twitter-api/tweets/filtered-stream/api-reference/get-tweets-search-stream
-    try:
-        client.filter(expansions=expansions, place_fields=place_fields, tweet_fields=tweet_fields, user_fields=user_fields, threaded=True)
-    except KeyboardInterrupt:
-        fp.close()
-        client.disconnect()
+    client.filter(expansions=expansions, place_fields=place_fields, tweet_fields=tweet_fields, user_fields=user_fields, threaded=True)
+
+if __name__ == "__main__":
+    """
+     - Save it in a secure location
+     - Treat it like a password or a set of keys
+     - If security has been compromised, regenerate it
+     - DO NOT store it in public places or shared docs
+    """
+    main()
