e27d46142e48cf21a3c645f267adf99cbb9cb92d
DavidL124 <60912784+DavidL124@users.noreply.github.com>
2022-04-29 02:36:28 +1000

    Update to post to CouchDB

--- reading_tweets/harvestor_draft2/run_all.sh
+++ reading_tweets/harvestor_draft2/run_all.sh
@@ -2,5 +2,5 @@
 python3 ./crawler.py
 
 #Post to CouchDB
-#curl -v -XPUT "http://user:pass@localhost:5984/tweets_draft2"
-#curl -XPOST "http://user:pass@localhost:5984/tweets_draft2/_bulk_docs" --header "Content-Type: application/json" --data @tweets_draft2.json
+curl -v -XPUT "http://user:pass@localhost:5984/tweets_draft5"
+curl -XPOST "http://user:pass@localhost:5984/tweets_draft5/_bulk_docs" --header "Content-Type: application/json" --data @tweets_draft2.json
