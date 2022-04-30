f124469be3a699ac1afdb35706a7001d6aa674c2
alex <atroup@student.unimelb.edu.au>
2022-04-30 17:15:14 +1000

    add loop to main for credential cycling

--- reading_tweets/harvester/main.py
+++ reading_tweets/harvester/main.py
@@ -12,6 +12,8 @@ import couchdb
 import sys
 from twitter.crawler import do_work
 from logger.logger import log
+from random import randint
+import time
 
 
 if __name__ == "__main__":
@@ -77,24 +79,41 @@ if __name__ == "__main__":
     doc = credentials_server["twitter_credentials"]
     twitter_credentials = None
 
-    # attempt to find the credentials by position. If this is server 0 and that was the ID passed in,
-    # then we will attempt to use the credentials at array position 0 in the twitter_credentials record in couchDB.
-    try:
-        twitter_credentials = doc["val"][args.credentials_id]
-    except IndexError:
-        log(f"No credentials object found at index {str(args.credentials_id)}", True)
-        sys.exit()  # cannot do anything further, so quit.
-    log(twitter_credentials, args.debug)
+    current_credential_index = args.credentials_id
 
-    # todo: convert to loop
-    if args.mode.lower() == "stream":
-        # do some streaming
-        # this will run until terminated, or an API error is encountered from which we cannot recover
-        log("streaming", args.debug)
-        result = do_work(twitter_credentials, args, couchdb_server, mode="stream")
-    elif args.mode.lower() == "search":
-        # do some searching
-        # this will also run until terminated or an API error etc.
-        log("searching", args.debug)
-        result = do_work(twitter_credentials, args, couchdb_server, mode="search")
-        # todo: handle an error such as running out of API space! We can cycle here
+    while True:
+
+        # attempt to find the credentials by position. If this is server 0 and that was the ID passed in,
+        # then we will attempt to use the credentials at array position 0 in the twitter_credentials record in couchDB.
+        try:
+            twitter_credentials = doc["val"][args.credentials_id]
+        except IndexError:
+            log(
+                f"No credentials object found at index {str(args.credentials_id)}", True
+            )
+            sys.exit()  # cannot do anything further, so quit.
+        log(twitter_credentials, args.debug)
+
+        # todo: convert to loop
+        if args.mode.lower() == "stream":
+            # do some streaming
+            # this will run until terminated, or an API error is encountered from which we cannot recover
+            log("streaming", args.debug)
+            result = do_work(twitter_credentials, args, couchdb_server, mode="stream")
+        elif args.mode.lower() == "search":
+            # do some searching
+            # this will also run until terminated or an API error etc.
+            log("searching", args.debug)
+            result = do_work(twitter_credentials, args, couchdb_server, mode="search")
+            # todo: handle an error such as running out of API space! We can cycle here
+
+        # the idea here is that if a rate limit error was returned, we can continue to cycle through credentials
+        # until we find some credentials that let us continue
+        # we add some jitter with a sleep function to prevent all crawlers using the same credential and erroring out at the same time or similar.
+        # note that this may not actually be recoverable, if all credentials have been exhausted. There is nothing
+        # we can do in that case and the crawler(s) will eventually all terminate.
+        current_credential_index += 1
+        log("sleeping before attempting with new credentials", args.debug)
+        time.sleep(randint(3, 20))
+        if current_credential_index >= len(doc["val"]):
+            sys.exit()
