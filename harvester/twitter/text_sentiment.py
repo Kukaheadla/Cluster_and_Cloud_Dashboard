b0821df14b151a74394b092567e94a3b47ab69ee
DavidL124 <60912784+DavidL124@users.noreply.github.com>
2022-05-01 18:19:10 +1000

    Text sentiment function

--- harvester/twitter/text_sentiment.py
+++ harvester/twitter/text_sentiment.py
@@ -1,1 +1,25 @@
-import textBlob
+'''
+Author: Kevin Yang
+'''
+
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import json, re, contractions
+
+def attach_sentiment(tweet_object):
+    analyzer = SentimentIntensityAnalyzer()
+    sentence = tweet_object['text']
+    #Remove url from string
+    sentence = re.sub(r'http\S+', '', sentence).strip()
+    #fix contractions
+    sentence = contractions.fix(sentence, slang=True)
+    #remove punctuation from string
+    sentence = re.sub(r'[^\w\s]','', sentence).strip()
+    #remove extra whitespace
+    sentence = re.sub(r' +', ' ', sentence).strip()
+    #lowercase everything
+    sentence = sentence.lower()
+
+    sentiment_dict = analyzer.polarity_scores(sentence)
+
+    tweet_object.update({"sentiments" : sentiment_dict})
+    return tweet_object
