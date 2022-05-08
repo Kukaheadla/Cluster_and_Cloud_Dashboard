e81293bdc7778654ffd853efecf790bae6c7a906
DavidL124 <60912784+DavidL124@users.noreply.github.com>
2022-05-08 14:24:51 +1000

    Incorporated conflicted code

--- harvester/main.py
+++ harvester/main.py
@@ -4,7 +4,7 @@ This file serves as the entry point to the crawler application.
 This file is essentially a dispatch point for various options relating to crawling Tweets.
 You can set various modes, and pass in particular relevant keys.
 
-Authors: Alex
+Authors: Alex, David
 """
 
 import argparse
@@ -16,6 +16,11 @@ from random import randint
 import time
 import tweepy
 from tweepy import OAuthHandler
+
+def add_list(author_id_lst, result):
+    for item in result:
+        if item not in author_id_lst:
+            author_id_lst.append(item)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("main")
@@ -41,9 +46,11 @@ if __name__ == "__main__":
         "--city", help="city topic, e.g. melbourne or sydney", type=str, required=True
     )
     parser.add_argument("--mode", help="e.g. stream or search", type=str, required=True)
-    
-    parser.add_argument("--topic", help="e.g. environment or transport", type=str, required=True)
-    
+
+    parser.add_argument(
+        "--topic", help="e.g. environment or transport", type=str, required=False
+    )
+
     parser.add_argument(
         "-o",
         help="specify an output file for returned Tweets.",
@@ -82,57 +89,124 @@ if __name__ == "__main__":
     credentials_server = couchdb_server["credentials"]
     twitter_credentials = None
     current_credential_index = args.credentials_id
+    result = None
+    credentials_count = 0
 
-    while True:
-        doc = credentials_server["twitter_credentials"]
-        log(
-            f"There are {str(len(doc['val']))} credential keys in the database",
-            args.verbose,
-        )
-        # attempt to find the credentials by position. If this is server 0 and that was the ID passed in,
-        # then we will attempt to use the credentials at array position 0 in the twitter_credentials record in couchDB.
-        try:
-            twitter_credentials = doc["val"][current_credential_index]
+    twitter_id_lst = []
+    author_id_lst = []
+    
+    total_tweets = 0
+    usr_count = 0
+    valid_tweets = 0
+    try:
+        while True:
+            doc = credentials_server["twitter_credentials"]
+            credentials_count = len(doc["val"])
             log(
-                f"using credentials with name: {doc['val'][current_credential_index]['name']}",
-                args.debug,
+                f"There are {str(len(doc['val']))} credential keys in the database",
+                args.verbose,
             )
-        except IndexError:
-            log(
-                f"No credentials object found at index {str(args.credentials_id)}", True
-            )
-            sys.exit()  # cannot do anything further, so quit.
-        log(twitter_credentials, args.debug)
+            # attempt to find the credentials by position. If this is server 0 and that was the ID passed in,
+            # then we will attempt to use the credentials at array position 0 in the twitter_credentials record in couchDB.
+            try:
+                twitter_credentials = doc["val"][current_credential_index]
+                log(
+                    f"using credentials with name: {doc['val'][current_credential_index]['name']}",
+                    args.debug,
+                )
+            except IndexError:
+                log(
+                    f"No credentials object found at index {str(args.credentials_id)}",
+                    True
+                )
+                sys.exit()  # cannot do anything further, so quit.
+            log(twitter_credentials, args.debug)
 
-        # todo: convert to loop
-        if args.mode.lower() == "stream":
-            # do some streaming
-            # this will run until terminated, or an API error is encountered from which we cannot recover
-            # if for example this hits the tweet quota for a developer account, the loop should cycle to new credentials
-            log("streaming", args.debug)
-            result = do_work(twitter_credentials, args, couchdb_server, mode="stream")
-        elif args.mode.lower() == "search":
-            # do some searching
-            # this will also run until terminated or an API error etc.
-            log("searching", args.debug)
-            result = do_work(twitter_credentials, args, couchdb_server, mode="search")
+            # todo: convert to loop
+            if args.mode.lower() == "stream":
+                # do some streaming
+                # this will run until terminated, or an API error is encountered from which we cannot recover
+                # if for example this hits the tweet quota for a developer account, the loop should cycle to new credentials
+                log("streaming", args.debug)
+                try:
+                    result = do_work(
+                        twitter_id_lst, 
+                        author_id_lst, 
+                        twitter_credentials, 
+                        args, 
+                        couchdb_server, 
+                        mode="stream", 
+                    )
+                    
+                    total_tweets += result[0]
+                    valid_tweets += result[1]
 
-        # the idea here is that if a rate limit error was returned, we can continue to cycle through credentials
-        # until we find some credentials that let us continue
-        # we add some jitter with a sleep function to prevent all crawlers using the same credential and erroring out at the same time or similar.
-        # note that this may not actually be recoverable, if all credentials have been exhausted. There is nothing
-        # we can do in that case and the crawler(s) will eventually all terminate.
-        current_credential_index += 1
-        log("sleeping before attempting with new credentials", args.debug)
+                    add_list(twitter_id_lst, result[2])
+                    add_list(author_id_lst, result[3])
 
-        # the random sleep time here is supposed to make it such that no two harvesters (in our cluster)
-        # will acquire the same credentials *at the same time*. It is not a _huge_ problem if two harvesters do this,
-        # but it may lead to unnecessary cycling between credentials.
-        time.sleep(randint(3, 20))
-        if current_credential_index >= len(doc["val"]):
-            print("length is", str(len(doc["val"])))
-            print("Exit")
-            # we have reached the end of the array
-            # aspirationally this could continue to cycle in a more interesting way
-            # however this is unlikely to be implemented
-            sys.exit()
+                except tweepy.errors.HTTPException:
+                    # probably a disconnect for misc. reasons, we can deal with this.
+                    current_credential_index -= (
+                        1  # keep the index the same on the next retry
+                    )
+                except Exception as e:
+                    log("line 140: exception occurred", True)
+                    log(str(e), args.debug)
+            elif args.mode.lower() == "search":
+                # do some searching
+                # this will also run until terminated or an API error etc.
+                log("searching", args.debug)
+                result = do_work(
+                    twitter_id_lst, 
+                    author_id_lst, 
+                    twitter_credentials, 
+                    args, 
+                    couchdb_server, 
+                    current_credential_index, 
+                    usr_count, 
+                    mode="search"
+                )
+
+                total_tweets += result[0]
+                valid_tweets += result[1]
+
+                twitter_id_lst.extend(result[2])
+                author_id_lst.extend(result[3])
+
+                usr_count = result[4]
+
+                #log("streaming", args.debug)
+                #try:
+                #    result = do_work(
+                #        twitter_id_lst, author_id_lst, twitter_credentials, args, couchdb_server, current_credential_index, len(doc["val"]), mode="stream")
+                #except tweepy.errors.HTTPException:
+                #    # probably a disconnect for misc. reasons, we can deal with this.
+                #    current_credential_index -= (
+                #        1  # keep the index the same on the next retry
+                #    )
+                #except Exception as e:
+                #    log(str(e), args.debug)
+
+            # the idea here is that if a rate limit error was returned, we can continue to cycle through credentials
+            # until we find some credentials that let us continue
+            # we add some jitter with a sleep function to prevent all crawlers using the same credential and erroring out at the same time or similar.
+            # note that this may not actually be recoverable, if all credentials have been exhausted. There is nothing
+            # we can do in that case and the crawler(s) will eventually all terminate.
+            current_credential_index += 1
+            log("sleeping before attempting with new credentials", args.debug)
+
+            # the random sleep time here is supposed to make it such that no two harvesters (in our cluster)
+            # will acquire the same credentials *at the same time*. It is not a _huge_ problem if two harvesters do this,
+            # but it may lead to unnecessary cycling between credentials.
+            time.sleep(randint(3, 20))
+            if current_credential_index >= len(doc["val"]):
+                log(
+                    "sleeping for 15 minutes as we likely encountered a rate limiting error or similar",
+                    args.debug,
+                )
+                time.sleep(15 * 60)
+                # start again!
+                current_credential_index = 0
+
+    except KeyboardInterrupt:
+        sys.exit()
