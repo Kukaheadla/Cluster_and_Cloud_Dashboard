bc655a1dc5394b17d8023ddc58168db8df8dd21d
alex <atroup@student.unimelb.edu.au>
2022-05-12 21:16:55 +1000

    fix helper starter script for dev web app launch in docker

--- web_application/docker_compose_start.sh
+++ web_application/docker_compose_start.sh
@@ -2,7 +2,7 @@
 # this file simply holds some commands that you might find useful
 
 # build and starts
-docker-compose -f docker-compose.prod.yml up -d --build
+docker-compose -f docker-compose.dev.yml up -d --build
 
 # kills and removes containers
 # docker-compose -f docker-compose.prod.yml down -v   
